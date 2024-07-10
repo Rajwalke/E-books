6990fc18fc2f48adf7288138efafb7190ac8e6df
RajWalke <rajwalke66@gmail.com>
2024-07-11 03:30:19 +0530

    semester 7 work is done..all the subject was adeed sucessfully

---
+++ SemesterSubject/sem7subject.js
@@ -0,0 +1,26 @@
+const list=document.querySelector('.listsignjs');
+const option=document.querySelector('.allmenuholder');
+list.addEventListener('click',()=>{
+    option.classList.toggle('active');
+
+})
+
+document.querySelector(".ML").addEventListener('click',()=>{
+    window.location.href="http://127.0.0.1:5500/semester7subject/sem7ml.html";
+})
+
+document.querySelector(".BDA").addEventListener('click',()=>{
+    window.location.href="http://127.0.0.1:5500/semester7subject/sem7bda.html";
+})
+document.querySelector(".NLP").addEventListener('click',()=>{
+    window.location.href="http://127.0.0.1:5500/semester7subject/sem7nlp.html";
+})
+document.querySelector(".BC").addEventListener('click',()=>{
+    window.location.href="http://127.0.0.1:5500/semester7subject/sem7bc.html";
+})
+document.querySelector(".CSL").addEventListener('click',()=>{
+    window.location.href="http://127.0.0.1:5500/semester7subject/sem7csl.html";
+})
+document.querySelector(".IR").addEventListener('click',()=>{
+    window.location.href="http://127.0.0.1:5500/semester7subject/sem7ir..html";
+})
