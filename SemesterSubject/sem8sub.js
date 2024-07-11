f8a93370f1f77a9eedb21c2232549767eb5d5dfd
RajWalke <rajwalke66@gmail.com>
2024-07-12 02:44:02 +0530

    some small mistakes are solved in subject 8

--- SemesterSubject/sem8sub.js
+++ SemesterSubject/sem8sub.js
@@ -6,20 +6,20 @@ list.addEventListener('click',()=>{
 })
 
 document.querySelector(".HMI").addEventListener('click',()=>{
-    window.location.href="";
+    window.location.href="http://127.0.0.1:5500/semester8subject/sem8HMI.html";
 })
 document.querySelector(".DC").addEventListener('click',()=>{
-    window.location.href="";
+    window.location.href="http://127.0.0.1:5500/semester8subject/sem8DC.html";
 })
 document.querySelector(".DL").addEventListener('click',()=>{
-    window.location.href="";
+    window.location.href="http://127.0.0.1:5500/semester8subject/sem8DL.html";
 })
 document.querySelector(".ADS").addEventListener('click',()=>{
-    window.location.href="";
+    window.location.href="http://127.0.0.1:5500/semester8subject/sem8ADS.html";
 })
 document.querySelector(".SMA").addEventListener('click',()=>{
-    window.location.href="";
+    window.location.href="http://127.0.0.1:5500/semester8subject/sem8SMA.html";
 })
 document.querySelector(".PM").addEventListener('click',()=>{
-    window.location.href="";
+    window.location.href="http://127.0.0.1:5500/semester8subject/sem8PM.html";
 })
