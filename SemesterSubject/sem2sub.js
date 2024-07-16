38482df925ff5c448a3294378c501d0aa423b8b3
RajWalke <rajwalke66@gmail.com>
2024-07-17 04:01:43 +0530

    some chnages are done a tag is added on subject 2

--- SemesterSubject/sem2sub.js
+++ SemesterSubject/sem2sub.js
@@ -6,21 +6,26 @@ list.addEventListener('click',()=>{
 })
 
 
-document.querySelector(".Mathematics2").addEventListener('click',()=>{
-    window.location.href="http://127.0.0.1:5500/semester2subject/sem2mathematics2.html";
-})
-document.querySelector(".Physics2").addEventListener('click',()=>{
-    window.location.href="http://127.0.0.1:5500/semester2subject/sem2physics2.html";
-})
-document.querySelector(".Chemistry2").addEventListener('click',()=>{
-    window.location.href="http://127.0.0.1:5500/semester2subject/sem2chemistry2.html";
-})
-document.querySelector(".Graphics").addEventListener('click',()=>{
-    window.location.href="http://127.0.0.1:5500/semester2subject/sem2graphics.html";
-})
-document.querySelector(".cprograaming").addEventListener('click',()=>{
-    window.location.href="http://127.0.0.1:5500/semester2subject/sem2cprograaming.html";
-})
-document.querySelector(".Communication").addEventListener('click',()=>{
-    window.location.href="http://127.0.0.1:5500/semester2subject/sem2communication.html";
-})
+// document.querySelector(".Mathematics2").addEventListener('click',()=>{
+//     window.location.href="http://127.0.0.1:5500/semester2subject/sem2mathematics2.html";
+// })
+// document.querySelector(".Physics2").addEventListener('click',()=>{
+//     window.location.href="http://127.0.0.1:5500/semester2subject/sem2physics2.html";
+// })
+// document.querySelector(".Chemistry2").addEventListener('click',()=>{
+//     window.location.href="http://127.0.0.1:5500/semester2subject/sem2chemistry2.html";
+// })
+// document.querySelector(".Graphics").addEventListener('click',()=>{
+//     window.location.href="http://127.0.0.1:5500/semester2subject/sem2graphics.html";
+// })
+// document.querySelector(".cprograaming").addEventListener('click',()=>{
+//     window.location.href="http://127.0.0.1:5500/semester2subject/sem2cprograaming.html";
+// })
+// document.querySelector(".Communication").addEventListener('click',()=>{
+//     window.location.href="http://127.0.0.1:5500/semester2subject/sem2communication.html";
+// })
+
+// const home=document.querySelector(".home");
+// home.addEventListener('click',()=>{
+    
+// })
