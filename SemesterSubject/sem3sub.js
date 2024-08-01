58f339484f1ac509732c9f29102d7b160673e055
RajWalke <rajwalke66@gmail.com>
2024-08-01 10:43:31 +0530

    updated code

--- SemesterSubject/sem3sub.js
+++ SemesterSubject/sem3sub.js
@@ -5,24 +5,24 @@ list.addEventListener('click',()=>{
 
 })
 
-document.querySelector(".Mathematics3").addEventListener('click',()=>{
-    window.location.href="http://127.0.0.1:5500/semester3subject/sem3maths3.html";
-});
+// document.querySelector(".Mathematics3").addEventListener('click',()=>{
+//     window.location.href="http://127.0.0.1:5500/semester3subject/sem3maths3.html";
+// });
 
 
-document.querySelector(".DSGT").addEventListener('click',()=>{
-    window.location.href="http://127.0.0.1:5500/semester3subject/sem3dsagt.html";
-})
+// document.querySelector(".DSGT").addEventListener('click',()=>{
+//     window.location.href="http://127.0.0.1:5500/semester3subject/sem3dsagt.html";
+// })
 
-document.querySelector(".DSA").addEventListener('click',()=>{
-    window.location.href="http://127.0.0.1:5500/semester3subject/sem3dsa.html";
-})
-document.querySelector(".DLCA").addEventListener('click',()=>{
-    window.location.href="http://127.0.0.1:5500/semester3subject/sem3dlca.html";
-})
-document.querySelector(".CG").addEventListener('click',()=>{
-    window.location.href="http://127.0.0.1:5500/semester3subject/sem3cg.html";
-})
-document.querySelector(".Java").addEventListener('click',()=>{
-    window.location.href="http://127.0.0.1:5500/semester3subject/sem3java.html";
-})
+// document.querySelector(".DSA").addEventListener('click',()=>{
+//     window.location.href="http://127.0.0.1:5500/semester3subject/sem3dsa.html";
+// })
+// document.querySelector(".DLCA").addEventListener('click',()=>{
+//     window.location.href="http://127.0.0.1:5500/semester3subject/sem3dlca.html";
+// })
+// document.querySelector(".CG").addEventListener('click',()=>{
+//     window.location.href="http://127.0.0.1:5500/semester3subject/sem3cg.html";
+// })
+// document.querySelector(".Java").addEventListener('click',()=>{
+//     window.location.href="http://127.0.0.1:5500/semester3subject/sem3java.html";
+// })
