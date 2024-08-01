0c826c42d5d2165757c2b918fc85894bb296f9a4
RajWalke <rajwalke66@gmail.com>
2024-08-01 10:43:54 +0530

    bug fix

--- semester3subject/semsub3.js
+++ semester3subject/semsub3.js
@@ -1,8 +1,8 @@
 document.querySelector(".back-arrow").addEventListener("click",()=>{
     document.querySelector(".back-arrowp1").innerHTML=`<i class="fa-solid fa-spinner fa-spin"></i>`;
     setTimeout(()=>{
-        window.location.href="http://127.0.0.1:5500/SemesterSubject/sem3sub.html";
-    },2000);
+        // window.location.href="http://127.0.0.1:5500/SemesterSubject/sem3sub.html";
+    },3000);
    
 })
 
